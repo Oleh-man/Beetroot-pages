b2670aff58c5d5350a6142fbdd57bacda85fd93d
Oleh Mamalyha <omamalyha@gmail.com>
2022-04-20 12:13:25 +0300

    finished first task

--- js/events/script/script.js
+++ js/events/script/script.js
@@ -9,5 +9,13 @@ body.addEventListener('keydown', function (event) {
         event.preventDefault();
         text.style.display = 'none'
         input.style.display = 'block';
+        input.focus();
+        input.textContent = text.textContent;
+        input.selectionStart = input.value.length; // focus on the end of the line
+    }else if ((event.code == 'Equal shiftKey' || event.code == 'NumpadAdd') && (event.ctrlKey || event.metaKey)) {
+        event.preventDefault();
+        text.style.display = 'block';
+        input.style.display = 'none';
+        text.textContent = input.value;
     }
 });
