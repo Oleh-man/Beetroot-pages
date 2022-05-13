2f22c1f218e35be89bc99bf6193f3a425f8164d0
Oleh Mamalyha <omamalyha@gmail.com>
2022-05-13 22:50:14 +0300

    lil bit better

--- js/weather/weather-script.js
+++ js/weather/weather-script.js
@@ -2,14 +2,16 @@ const main = document.getElementById('main');
 const city = document.querySelector('.border');
 
 main.addEventListener('click', function (e) {
-    chosenCity = e.target.children[0].textContent; //get city name (str)
+    // console.dir(e);
+    let chosenCity = e.path[3].children[0].textContent;
+    // console.log(chosenCity)
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=21edd3c2f4e30b2f7d738b8251326cc5`)
     .then((result) => (result.json()))
     .then(data => {
         console.dir(data);
         getDegrees(data);
-        e.target.children[1].children[0].innerHTML = `${Math.floor(data.main.temp - 272.15)} &degС;`
+        e.path[1].innerHTML = `${Math.floor(data.main.temp - 272.15)} &degС;`
     });
 });
 
