294521f2c4eb8dbfef4879eb4441dd728d82fc4a
Oleh Mamalyha <omamalyha@gmail.com>
2022-05-16 23:16:07 +0300

    start count some food

--- burger/src/compnents/Builder/Builder.js
+++ burger/src/compnents/Builder/Builder.js
@@ -4,12 +4,13 @@ import './Builder.css'
 function Builder() {
     return (
         <div className="builder">
-            <BurgerItem/>
-            <BurgerItem/>
-            <BurgerItem/>
-            <BurgerItem/>
-            <BurgerItem/>
-            <BurgerItem/>
+            <BurgerItem itemName={'Meat'} itemPrice={5}/>
+            <BurgerItem itemName={'Chease'} itemPrice={3}/>
+            <BurgerItem itemName={'Salad'} itemPrice={2.5}/>
+            <BurgerItem itemName={'Tomatoes'} itemPrice={1.75}/>
+            <BurgerItem itemName={'Pickles'} itemPrice={2.25}/>
+            <BurgerItem itemName={'Bacon'} itemPrice={4}/>
+            <div className="total-price">Total = 0</div>
             <button>Checkout</button>
         </div>
     );
--- burger/src/compnents/Builder/Burger-item.js
+++ burger/src/compnents/Builder/Burger-item.js
@@ -1,15 +1,55 @@
+import React from 'react'
 import './Burger-item.css'
 
-function BurgerItem() {
-    
-    return (
-        <div className="builder__item">
-                <p className="builder__item--name"></p>
-                <button className="item--remove-btn">-</button>
-                <span className="item--count">0</span>
-                <button className="item--add-btn">+</button>
-        </div>
-    )
+class BurgerItem extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: props.itemName,
+            price: props.itemPrice,
+            count: 0,
+            countPrice: 0
+        }
+    }
+
+    addItem = () => {
+        this.setState((oldState) => {
+            if(oldState.count < 5) {
+                return {
+                    ...oldState,
+                    count: oldState.count + 1,
+                    countPrice: oldState.countPrice + oldState.price
+                }          
+            } 
+        })
+    }
+    removeItem = () => {
+        this.setState((oldState) => {
+            if(oldState.count <= 5 && oldState.count > 0) {
+                return {
+                    ...oldState,
+                    count: oldState.count - 1,
+                    countPrice: oldState.countPrice - oldState.price
+
+                }          
+            } 
+        })
+    }
+
+
+    render() {
+        return (
+            <div className="builder__item">
+                    <p className="builder__item--name">{this.state.name} - <span>{this.state.price}$</span></p>
+                    <button className="item--remove-btn" onClick={this.removeItem}>-</button>
+                    <span className="item--count">{this.state.count}</span>
+                    <button className="item--add-btn" onClick={this.addItem}>+</button>
+                    <p className="count-price">{this.state.countPrice}$</p>
+                    
+            </div>
+        )
+    }
+
 }
 
 export default BurgerItem;
