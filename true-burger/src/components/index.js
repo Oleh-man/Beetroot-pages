96bcc3ee1c21c699883e87f00cbb56ae8b59faf1
Oleh Mamalyha <91564806+Oleh-man@users.noreply.github.com>
2022-05-23 22:06:56 +0300

    Delete true-burger directory

--- true-burger/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header'
-export { default as Footer } from './Footer'
-export { default as Prices } from './Prices'
-export { default as Controls } from './Controls'
-export { default as Builder } from './Builder'
