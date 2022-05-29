aabef801ff9ba98045b118c4ae0844bdb29c1f69
Oleh Mamalyha <omamalyha@gmail.com>
2022-05-29 13:43:40 +0300

    starting work with api data

--- weather/src/components/CurrentWeather.js
+++ weather/src/components/CurrentWeather.js
@@ -1,19 +1,21 @@
-const CurrentWeather = () => {
+const CurrentWeather = (props) => {
     return (
         <div className="weather-container">
             <div className="weather-container__icon">soon</div>
-            <div className="weather-container__temp">
-                <span>102</span>
-                <div>
-                    <span>80</span>
-                    <span>/</span>
-                    <span>120</span>
-                </div>
+            <div className="weather-container__main-temp">
+                <div className="weather-container__main-temp--big">{props.data && props.data.main.temp}</div>
+                <div className="weather-container__main-temp--small">80/120</div>
             </div>
             <div className="weather-container__info">
-                <div>
-                    <span>icon</span>
-                    <span>5<span>km/h</span></span>
+                <div className="weather-container__info__icons">
+                    <div>icon</div>
+                    <div>icon</div>
+                    <div>icon</div>
+                </div>
+                <div className="weather-container__info__dem">
+                    <div>2kmh</div>
+                    <div>70%</div>
+                    <div>30%</div>
                 </div>
             </div>
         </div>
--- weather/src/containers/Main.js
+++ weather/src/containers/Main.js
@@ -5,31 +5,77 @@ class Main extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            city: '',
+            weatherData: null
         }
     }
 
     changeInputSize = (e) => {
-        console.dir(e.target)
-        console.log(e.target.value)
-        e.target.style.width = `${e.target.value.length}ch`
-        if (!e.target.value || e.target.value === '') {
-            e.target.style.width = `9.5ch`
+        this.setState((oldState) => {
+            return {
+                ...oldState,
+                city: e.target.value
+            }
+        })
+        
+        switch (e.target.value.length) {
+            case 1:
+                e.target.style.width = `${e.target.value.length + 1}ch`;
+                break;
+            case 2:
+                e.target.style.width = `${e.target.value.length + 2}ch`;
+                break;
+            case 0:
+                e.target.style.width = `9.5ch`;
+                break;
+        
+            default:
+                e.target.style.width = `${e.target.value.length + 1}ch`
+                break;
         }
     }
 
+    checkWeather = () => {
+        if (this.state.city !=='') {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=21edd3c2f4e30b2f7d738b8251326cc5`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState((oldState) => {
+                    return {
+                        ...oldState,
+                        weatherData: data
+                    }
+                })
+            })
+        }
+    }
+    
     render() {
+
+        console.log(this.state.weatherData)
+        
         return (
             <main className="wrapper">
                 <h1>Weather Checker</h1>
                 <div className='input-container'>
-                    <p className='input-container__question'>Want to know Weather in <input onChange={this.changeInputSize} className='city-name' type="text" placeholder='(your place)' autoFocus/> ?</p>
+                    <p className='input-container__question'>
+                        Want to know Weather in 
+                        <input 
+                        onChange={this.changeInputSize} 
+                        className='city-name' 
+                        type="text" 
+                        placeholder='(your place)' 
+                        autoFocus/> ?</p>
                     <div className='input-container__check'>
                         <span className='input-container__check--span'>Just</span>
-                        <button className='input-container__check--btn'>Check it</button>
+                        <button 
+                        className='input-container__check--btn' 
+                        onClick={this.checkWeather}>
+                        Check it</button>
                     </div>
                 </div>
-                <CurrentWeather />
+                {this.state.weatherData === null && <div className=''>Please enter city name and hit "Check it" button</div>}
+                {this.state.weatherData && <CurrentWeather data={this.state.weatherData}/>}
             </main>
         )
     }
