1111ecc61d117b3bcb68e21058254a2006b967ce
Oleh Mamalyha <omamalyha@gmail.com>
2022-06-02 00:21:17 +0300

    done with degrees switch, add main icon

--- weather/src/components/CurrentWeather.js
+++ weather/src/components/CurrentWeather.js
@@ -1,25 +1,29 @@
 const CurrentWeather = (props) => {
     return (
-        <div className="weather-container">
-            <div className="weather-container__icon">soon</div>
-            <div className="weather-container__main-temp">
-                <div className="weather-container__main-temp--big">{props.data && props.data.main.temp}</div>
-                <div className="weather-container__main-temp--small">80/120</div>
-            </div>
-            <div className="weather-container__info">
-                <div className="weather-container__info__icons">
-                    <div>icon</div>
-                    <div>icon</div>
-                    <div>icon</div>
+        <>
+            <div className="weather-container">
+                <div className="weather-container__icon">
+                    <img src={props.data && "http://openweathermap.org/img/wn/" + props.data.weather[0].icon + "@4x.png"} alt="" />
                 </div>
-                <div className="weather-container__info__dem">
-                    <div>2kmh</div>
-                    <div>70%</div>
-                    <div>30%</div>
+                <div className="weather-container__main-temp">
+                    <div className="weather-container__main-temp--big">{props.data && props.toggleSystem(props.data.main.temp)}</div>
+                    <div className="weather-container__main-temp--small">{props.data && props.toggleSystem(props.data.main.temp_min)}/{props.data && props.toggleSystem(props.data.main.temp_max)}</div>
+                </div>
+                <div className="weather-container__info">
+                    <div className="weather-container__info__icons">
+                        <div>icon</div>
+                        <div>icon</div>
+                        <div>icon</div>
+                    </div>
+                    <div className="weather-container__info__dem">
+                        <div>2kmh</div>
+                        <div>70%</div>
+                        <div>30%</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        </>
+    );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
--- weather/src/containers/Main.js
+++ weather/src/containers/Main.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import CurrentWeather from '../components/CurrentWeather'
+import SwitchSystem from '../components/SwitchSystem'
 
 class Main extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             city: '',
-            weatherData: null
-        }
-    }
+            weatherData: null,
+            metric: true
+        };
+    };
 
     changeInputSize = (e) => {
         this.setState((oldState) => {
             return {
                 ...oldState,
                 city: e.target.value
-            }
-        })
-        
+            };
+        });
+
         switch (e.target.value.length) {
             case 1:
                 e.target.style.width = `${e.target.value.length + 1}ch`;
@@ -28,54 +30,77 @@ class Main extends React.Component {
             case 0:
                 e.target.style.width = `9.5ch`;
                 break;
-        
+
             default:
                 e.target.style.width = `${e.target.value.length + 1}ch`
                 break;
-        }
-    }
+        };
+    };
 
     checkWeather = () => {
-        if (this.state.city !=='') {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=21edd3c2f4e30b2f7d738b8251326cc5`)
-            .then(res => res.json())
-            .then(data => {
-                this.setState((oldState) => {
-                    return {
-                        ...oldState,
-                        weatherData: data
-                    }
-                })
-            })
-        }
-    }
-    
+        if (this.state.city !== '') {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=21edd3c2f4e30b2f7d738b8251326cc5&units=metric`)
+                .then(res => res.json())
+                .then(data => {
+                    this.setState((oldState) => {
+                        return {
+                            ...oldState,
+                            weatherData: data
+                        };
+                    });
+                });
+        };
+    };
+
+    changeTempSystem = () => {
+        this.setState((oldState) => {
+            return {
+                ...oldState,
+                metric: !oldState.metric
+            };
+        });
+    };
+
+    toggleSystem = (temp) => {
+        if (this.state.metric === false) {
+            return Math.floor((temp * 1.8) + 32);
+        } else {
+            return Math.floor(temp);
+        };
+    };
+
     render() {
 
         console.log(this.state.weatherData)
-        
+
         return (
             <main className="wrapper">
                 <h1>Weather Checker</h1>
                 <div className='input-container'>
                     <p className='input-container__question'>
-                        Want to know Weather in 
-                        <input 
-                        onChange={this.changeInputSize} 
-                        className='city-name' 
-                        type="text" 
-                        placeholder='(your place)' 
-                        autoFocus/> ?</p>
+                        Want to know Weather in
+                        <input
+                            onChange={this.changeInputSize}
+                            className='city-name'
+                            type="text"
+                            placeholder='(your place)'
+                            autoFocus /> ?</p>
                     <div className='input-container__check'>
                         <span className='input-container__check--span'>Just</span>
-                        <button 
-                        className='input-container__check--btn' 
-                        onClick={this.checkWeather}>
-                        Check it</button>
+                        <button
+                            className='input-container__check--btn'
+                            onClick={this.checkWeather}>
+                            Check it</button>
                     </div>
                 </div>
                 {this.state.weatherData === null && <div className=''>Please enter city name and hit "Check it" button</div>}
-                {this.state.weatherData && <CurrentWeather data={this.state.weatherData}/>}
+                {<CurrentWeather
+                    data={this.state.weatherData}
+                    ifMetric={this.state.metric}
+                    changeSystem={this.changeSystem}
+                    toggleSystem={this.toggleSystem} />}
+                {/* {this.state.weatherData && <CurrentWeather data={this.state.weatherData} ifMetric={this.state.metric}/>} */}
+                <SwitchSystem changeTempSystem={this.changeTempSystem} />
             </main>
         )
     }
